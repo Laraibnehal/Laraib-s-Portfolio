30e23f988c6bb03d35b5a45012a4fd0b3abf5871
Laraib <laraibmallick7@gmail.com>
2025-03-04 16:55:18 +0530

    home updated

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,8 +4,8 @@ import TextChange from "../TextChange";
 
 const Home = () => {
   return (
-    <div className="text-black flex w-full justify-between items-start p-10 md:p-20 ">
-      <div className="md:w-2/4 md:pt-10 ">
+    <div className="text-black flex w-full justify-between items-start p-4 md:p-20 ">
+      <div className="md:w-2/4 w-40 md:pt-10 ">
         <h1 className="text-xl md:text-6xl font-bold flex leading-normal tracking-tighter">
           <TextChange />
         </h1>
@@ -13,12 +13,12 @@ const Home = () => {
         a versatile website developer, freelancer, and perfume business owner based in Kolkata, India, passionate about blending technology and creativity.
         </p>
        
-        <button className="mt-5 md:md-10 text-black py02 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#badbf7]">
+        <button className="mt-5 md:md-10 text-black py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#badbf7]">
          <a href="#Footer">Contact me</a>
         </button>
       </div>
-      <div className="w-[50%] h-[50%]">
-        <img className=" m-12" src={avatarImg} alt="" />
+      <div className="md:w-[50%]  md:h-[50%] w-[70%] h-[70%]">
+        <img className=" m-5 md:m-12" src={avatarImg} alt="" />
       </div>
     </div>
   );
